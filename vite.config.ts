379aa23d79346bed07dae1871c1133640c852120
HYF <1151665922@qq.com>
2024-06-17 14:24:36 +0800

    Add vue build tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Icons from "unplugin-icons/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import VueComponents from "unplugin-vue-components/vite";
+import VueMarkdown from "unplugin-vue-markdown/vite";
+import VueRouter from "unplugin-vue-router/vite";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
-
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
@@ -18,4 +23,47 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("src", import.meta.url)),
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: "src/typings/auto-imports.d.ts",
+      include: [
+        /\.ts$/, // .ts
+        /\.vue$/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: [
+        "vue",
+        "vue-i18n",
+        "vue-router",
+        "@vueuse/core",
+        "@vueuse/head",
+        "@vueuse/math",
+        "pinia",
+        {
+          "naive-ui": ["useDialog", "useMessage", "useNotification", "useLoadingBar"],
+        },
+      ],
+    }),
+    VueComponents({
+      dts: "src/typings/components.d.ts",
+      dirs: "src/components",
+      extensions: ["vue", "md"],
+      resolvers: [NaiveUiResolver()],
+    }),
+    VueRouter({
+      dts: "src/typings/typed-router.d.ts",
+      extensions: [".vue", ".md"],
+      routesFolder: "src/pages",
+    }),
+    VueMarkdown({}),
+    Icons({
+      compiler: "vue3",
+    }),
+  ],
 }));
